ffac4be2ee49ad623f6e273d12e6e38ace62a1ab
hirako74 <t.hirako@gmail.com>
2022-11-13 16:22:24 +0900

    ローカルストレージ利用

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,9 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const todoRef = ref('');
+const todoListRef = ref([]);
+const addTodo = () => {
+  console.log(todoRef.value);
+  const id = new Date().getTime();
+  todoListRef.value.push({ id: id, task: todoRef.value });
+  localStorage.todoList = JSON.stringify(todoListRef.value);
+  todoRef.value = '';
+};
+</script>
 
 <template>
   <div class="box_input">
-    <input type="text" class="todo_input" placeholder="+ TODOを入力" />
-    <button class="btn">追加</button>
+    <input
+      type="text"
+      class="todo_input"
+      v-model="todoRef"
+      placeholder="+ TODOを入力"
+    />
+    <button class="btn" @click="addTodo">追加</button>
   </div>
 </template>
 
